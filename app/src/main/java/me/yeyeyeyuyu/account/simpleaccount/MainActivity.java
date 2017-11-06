841909ef14aa964362041b784fb02882413bc564
unknown <yeyu456@gmail.com>
2017-11-07 01:24:19 +0800

    init

---
+++ app/src/main/java/me/yeyeyeyuyu/account/simpleaccount/MainActivity.java
@@ -0,0 +1,12 @@
+package me.yeyeyeyuyu.account.simpleaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
